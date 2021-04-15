39e5c5df1c7a31e2df8c9fff604d6abb17891860
essneider07 <essneidervalderrama@gmail.com>
2021-04-15 13:20:32 -0500

    commit inicial

--- ejercicio_58.py
+++ ejercicio_58.py
@@ -1,7 +1,6 @@
-n = int(input("ingresa la cantidad de numeros :  "))
-suma_impares,suma_pares = 0,0
-for i  in range(1,n+1):
-    if i % 2 == 0:
-        print(i,"par")
-    if i % 2 != 0 :
-        print(i,"impar")
+n = int(input("ingresa un numero"))
+s = 0
+for i in range (1,n+1):
+    for j in range (1,i+1):
+        print(j,end="")
+    print()
